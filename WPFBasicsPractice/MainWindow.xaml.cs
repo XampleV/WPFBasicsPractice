b04c6a8b66d4d6b6bdc572b2d5168a091ca3fcc5
Mohammad Alshoubaki <mohammad.z.alshoubaki@gmail.com>
2020-09-08 13:08:56 -0400

    added sqlite functions

--- WPFBasicsPractice/MainWindow.xaml.cs
+++ WPFBasicsPractice/MainWindow.xaml.cs
@@ -21,17 +21,27 @@ namespace WPFBasicsPractice
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SQLiteConnection sqlite_conn;
+        private SQLiteCommand sqlite_cmd;
+
+
+        public string fullNameVar { get; private set; }
+        public string phoneVar { get; private set; }
+        public string dobVar { get; private set; }
+        public string addNotesVar { get; private set; }
+
         public MainWindow()
         {
+            createConnection();
             InitializeComponent();
         }
 
         private bool checkFields()
         {
-            string fullNameVar = fullName.Text;
-            string phoneVar = phoneNum.Text;
-            string dobVar = dob.Text;
-            string addNotesVar = addNotes.Text;
+            fullNameVar = fullName.Text;
+            phoneVar = phoneNum.Text;
+            dobVar = dob.Text;
+            addNotesVar = addNotes.Text;
 
             if (fullNameVar == "" || phoneVar == "" || dobVar == "")
             {
@@ -48,9 +58,49 @@ namespace WPFBasicsPractice
                 MessageBox.Show("Please input all required fields.", "Error");
                 return;
             }
+            insertIntoDatabase(fullNameVar, phoneVar, dobVar, addNotesVar);
+
             
-            // here is true, we can insert into local db
-            
+        }
+        private bool insertIntoDatabase(string name, string phone, string dob, string notes)
+        {
+            try
+            {
+                sqlite_cmd.CommandText = "INSERT INTO Clients (name, phone, dob, notes) VALUES(@name, @phone, @dob, @notes)";
+                sqlite_cmd.Parameters.AddWithValue("@name", name);
+                sqlite_cmd.Parameters.AddWithValue("@phone", phone);
+                sqlite_cmd.Parameters.AddWithValue("@dob", dob);
+                sqlite_cmd.Parameters.AddWithValue("@notes", notes);
+
+                sqlite_cmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully inserted client.", "Success");
+                return true;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Failed to insert user.\nError: {err}", "Error");
+                return false;
+            }
+
+        }
+        private void createConnection()
+        {
+            if (System.IO.File.Exists("database.db"))
+            {
+                sqlite_conn = new SQLiteConnection("DataSource=database.db;Version=3;");
+                sqlite_conn.Open();
+                sqlite_cmd = sqlite_conn.CreateCommand();
+            }
+            else
+            {
+                sqlite_conn = new SQLiteConnection("Data Source=database.db;Version=3;New=True;Compress=True;");
+                sqlite_conn.Open();
+                sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_cmd.CommandText = "CREATE TABLE Clients (name string primary key, phone string, dob string, notes string)";
+                sqlite_cmd.ExecuteNonQuery();
+                MessageBox.Show("Database was successfully created.", "Success");
+            }
+        
         }
     }
 }
